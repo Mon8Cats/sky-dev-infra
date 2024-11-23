21e6439f8a97af7c1c6f9f76c449450bef798ce3
SteveKimMHS <Steve.Kim@MHShealth.com>
2024-11-23 13:33:17 -0600

    store secrets

--- infrastructure/s02_all_secrets/main.tf
+++ infrastructure/s02_all_secrets/main.tf
@@ -1,10 +1,11 @@
 /* No space around =
-export TF_VAR_project_id="mon-cloud-lab"
-export TF_VAR_project_number="583630435909"
+export TF_VAR_project_id="sky-root"
+export TF_VAR_project_number="262659146932"
 export TF_VAR_region="us-central1"
 echo $TF_VAR_project_id
 echo $TF_VAR_region
 */
+
 
 # (1) enable apis
 module "enable_apis" {
@@ -15,28 +16,30 @@ module "enable_apis" {
 
 # (5) secrete manager
 module "github_token_secret" {
-  source              = "../modules/c05_secret_manager"
+  source              = "../../modules/c05_secret_manager"
   project_id          = var.project_id
   secret_id         = var.secret_id_github
 }
 
 module "secret_db_user" {
-  source              = "../modules/c05_secret_manager"
+  source              = "../../modules/c05_secret_manager"
   project_id          = var.project_id
   secret_id         = var.secret_id_db_user
 }
 
 module "secret_db_password" {
-  source              = "../modules/c05_secret_manager"
+  source              = "../../modules/c05_secret_manager"
   project_id          = var.project_id
   secret_id         = var.secret_id_db_password
 }
 
 
 module "github_token_secret_access_cloud_build_sa" {
-  source              = "../modules/c06_secret_access"
+  source              = "../../modules/c06_secret_access"
   secret_id = var.secret_id_github
   service_account_email = local.cloud_build_service_account_email
+
+  depends_on = [ module.enable_apis, module.github_token_secret ]
 }
 
 
--- infrastructure/s03_store_secrets/terraform.tfvars
+++ infrastructure/s03_store_secrets/terraform.tfvars
@@ -1,17 +1,16 @@
-# (1) project id
+# project id
 #project_id = "sky-root"
 #project_number = "650839457214"
 #region     = "my region" # "us-central1"
 #bucket_name = "mon8cats-lab-tf-backend" # unique
 
 
-# (2) api list
-api_list   = [
-    "secretmanager.googleapis.com", # manually enabled
-    "cloudbuild.googleapis.com",
-  ]
-
-# (5) Secret
+# secret
 secret_id_github = "github_token"
 secret_id_db_user = "db_user"
 secret_id_db_password = "db_password"
+
+
+secret_value_github = "github_token"
+secret_value_db_user = "db_user"
+secret_value_db_password = "db_password"
